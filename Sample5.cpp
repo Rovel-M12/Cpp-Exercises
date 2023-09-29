6ce1427584e140e5d15978cafe10240e6681387e
Rovel May F. Almonguera <146396094+Rovel-M12@users.noreply.github.com>
2023-09-29 08:19:13 +0800

    Add files via upload

---
+++ Sample5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+ double radius, pi=3.14, r;
+ 
+ cout<<"radius: ";
+ cin>>radius;
+ 
+
+ 
+ r = radius*radius ;  
+ cout<<"The area of circle is: "<<pi *r;  
+	
+	return 0;
+}
---
+++ Sample6.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std;
+
+int main (){
+	
+	   
+	   double x, y, sum;
+	    
+	    cout<<"Enter Salary Per Day: ";
+	    cin>>x;
+	    
+	    cout<<"Enter number of Days Work: ";
+	    cin>>y;
+	    
+	    sum = x * y;
+	    
+	    cout<<"Your total Salary is: "<<sum;
+
+  return 0;	
+}
+
+

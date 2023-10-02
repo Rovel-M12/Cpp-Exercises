e850cff7943490ed70a7ac740a15fe20a836a65a
Rovel May F. Almonguera <146396094+Rovel-M12@users.noreply.github.com>
2023-10-02 13:57:12 +0800

    Add files via upload

---
+++ Sample10.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+using namespace std;
+
+int main (){
+	
+  int x;
+
+   cout <<"Enter your grade:";
+   cin>>x;
+
+   if(x >=75){
+   
+   cout <<"Passed";
+
+   }
+   else{
+   
+   cout << "Failed";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+  return 0;	
+}
+
+
